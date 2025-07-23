aaecb3837ce2e0804901f975dd14fe001a2eb066
freewha <freewha@kdwz.cloudns.ph>
2025-07-23 13:54:16 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytdlala"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
+USER_NAME = "k3ic6gSll0pLNGP6IrC0v2c4RDPjo"         # 用户名
+UUID = "84a4d67a-c8a5-442a-b16f-125adfe6dcdd"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "uhsy-cdhcnfd-cvmnhshdhxc-cvceceka.ubhh7bbs.ip-ddns.com"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiNmRmNWUwMTAwOGU1NGE2OGFmMDdhZDZjMTE2MDRmNWUiLCJ0IjoiZDkxZWY1ZjctNmUyNC00ZjNhLWFjYTEtNTIwMjM4OTgwZWIzIiwicyI6Ik1HTTJNMlF6WTJNdFpEYzBaUzAwTVRWbExXRXdPR0l0TURreU5XSXpNMk15TVRKaiJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
